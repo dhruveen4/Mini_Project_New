aea2dac6f943e548f8a14e1ab5afb5f03bf81bf2
Dhruveen <dnp49@njit.edu>
2018-10-06 19:27:36 -0400

    Task : Testing Environment

--- public/index.php
+++ public/index.php
@@ -1,8 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bharvee23
- * Date: 06-10-2018
- * Time: 01:22 AM
- */
-echo 'test12345';
+main::start();
+class main {
+    static public function start(){
+        print 'Hello World';
+    }
+}
