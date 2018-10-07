bdb5a17743922c99d3085707c931a125daf0b00a
Dhruveen <dnp49@njit.edu>
2018-10-06 21:47:29 -0400

    Task : Editing Classes

--- public/index.php
+++ public/index.php
@@ -1,14 +1,58 @@
 <?php
 main::start("SalesJan2009.csv");
 class main{
-    static public function start($filename){
-        $records = csv::getRecords($filename);
-        $table = html::generateTable($records);
+    static public function start($SalesJan2009){
+        $records = csv::getRecords($SalesJan2009);
+        $table = html::generateTable($SalesJan2009);
         echo $table;
     }
 }
-class html{
 
+
+class html{
+    public static function
+    generateTable($records) {
+        $html = '<html>';
+        $html = html_hreader::getHtmlHeader();
+        $html = html_body::open_HtmlBody();
+
+        //Start Table
+        $html .= html_table::open_htmlTable();
+// Header Row
+//    $html .= '<tr>';
+
+        $count = 0;
+
+
+        foreach ($records as $record){
+
+//        if($count == 0){
+
+
+            $array = $record->returnArray();
+            $fields = array_keys($array);
+            $values = array_values($array);
+
+            while ($count == 0){
+
+                $html .= html_tableHead::open_TableHead();
+                $html .= create_table_Rows::open_tableRow();
+
+
+//            print_r($for);
+//            $html1  = create_table_Header::createHeader($for);
+//            $html .= $html1;
+
+
+                foreach ($fields as $value) {
+                    $html .= create_table_Header::createHeader($value);
+                }
+                $html .= create_table_Rows::close_tableRow();
+                $html .= html_tableHead::close_TableHead();
+
+                $count++;
+
+            }
 }
 
 class create_table_Headers{
@@ -20,6 +64,22 @@ class create_table_Rows{
 }
 
 class tableData{
+
+}
+
+class html_header{
+
+}
+
+class html_body{
+
+}
+
+class html_table{
+
+}
+
+class html_tablehead{
 
 }
 
